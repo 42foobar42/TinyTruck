41b4c19838e17e608dd462aff64f2648cae54f3b
Alexander Boll <alexander.boll@mni.thm.de>
2015-03-13 10:59:43 +0100

    added type of cargo a truck can carry (data.type) and if a truck can have trailers (data.trailers) #25

--- js/truckParts.js
+++ js/truckParts.js
@@ -7,7 +7,7 @@
 var Trucks = [
     {type: "truck", name: "Flatbed", id: 0, costs:1000, speed:80,
         parts: [{type: "Engine", costs: 500}, {type: "Axis", costs: 750}, {type: "Drivers cab", costs: 1000}],
-        data: {capacity: 10, speed: 100, fuel: 200}}
+        data: {capacity: 10, speed: 100, fuel: 200, type: "ordinary", trailers:0}}
     ,
     {type: "trailer", name: "Two-axle drawbar trailer", id:0, costs:700,
         parts: [{type: "Chassis", costs: 200}, {type: "Axis", costs: 250}, {type: "Platform", costs: 400}],
