cc39eddd9e2bbba2192c4d6046f0e09f73d6d229
Alexander Boll <alexander.boll@mni.thm.de>
2015-03-14 15:26:09 +0100

    initial goods data for example #10 moved data js files to sperate folder

---
+++ js/data/goods.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * tpyes: ordinary =>,
+ * 
+ */
+var Goods = [
+    { name:'lemonade', type:'ordinary', price: 100 },
+    { name:'pants', type:'ordinary', price: 500 },
+    { name:'shirts', type:'ordinary', price: 500 }
+]
